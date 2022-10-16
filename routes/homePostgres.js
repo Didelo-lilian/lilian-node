210c5dd942ffbfe3861065e07654431bb3f8ab54
SOLER Lilian <soler.lilian64@gmail.com>
2022-10-16 11:33:32 +0200

    Ajout CRUD home

--- routes/homePostgres.js
+++ routes/homePostgres.js
@@ -5,7 +5,10 @@ const homePostgresCtrl = require("../controllers/homePostgres");
 const auth = require("../middleware/auth");
 
 router
+  .post("/", homePostgresCtrl.createHome)
   .get("/", homePostgresCtrl.getHome)
+  .put("/", homePostgresCtrl.updateHome)
+  .delete("/", homePostgresCtrl.deleteHome);
   ;
 
 module.exports = router;
