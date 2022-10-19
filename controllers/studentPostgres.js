f7856a91cf4e1b9f942aec6e02094046a060cde4
SOLER Lilian <soler.lilian64@gmail.com>
2022-10-19 11:51:38 +0200

    Create, delete and get student with postgres

---
+++ controllers/studentPostgres.js
@@ -0,0 +1,113 @@
+const pool = require('../queries');
+
+exports.createStudent = (req, res) => {
+	if(!req.body.name && req.body.level){
+		res.status(400).json({ message : "Please provide a name and a level" });
+		return;
+	}
+
+	pool.query('INSERT INTO students (name, level) VALUES ($1, $2)', [req.body.name, req.body.level], (error, results) => {
+		if (error) {
+			res.status(400).json({error: error});
+			return;
+		}
+		res.status(201).json( {message : `Student added with name: ${req.body.name}, level: ${req.body.level}` })
+	});
+}
+
+exports.getStudents = (req, res) => {
+	console.log(req.params);
+	if(req.params.name && req.params.name == "all" && req.params.level && req.params.level == "all" || (!req.params.name && !req.params.level)){
+		pool.query('SELECT * FROM students', (error, results) => {
+			if (error) {
+				res.status(400).json({error: error});
+				return;
+			}
+			if(results.rows.length == 0) {
+				res.status(404).json({ message : "No students found" });
+				return;
+			}
+			res.status(200).json(results.rows);
+		});
+		return;
+	}
+
+	if(req.params.level && (req.params.name && req.params.name == "all")){
+		pool.query('SELECT * FROM students WHERE level = $1', [req.params.level], (error, results) => {
+			if (error) {
+				res.status(400).json({error: error});
+				return;
+			}
+			if(results.rows.length == 0){
+				res.status(404).json({message: "Student(s) with level " + req.params.level + " not found"});
+				return;
+			}
+			res.status(200).json(results.rows);
+		});
+		return;
+	}
+
+	if(req.params.name && (req.params.level && req.params.level == "all")){
+		pool.query('SELECT * FROM students WHERE name = $1', [req.params.name], (error, results) => {
+			if (error) {
+				res.status(400).json({error: error});
+				return;
+			}
+			if(results.rows.length == 0){
+				res.status(404).json({message: "Student(s) with name " + req.params.name + " not found"});
+				return;
+			}
+			res.status(200).json(results.rows);
+		});
+		return;
+	}
+
+	if(req.params.name && req.params.level){
+		pool.query('SELECT * FROM students WHERE name = $1 AND level = $2', [req.params.name, req.params.level], (error, results) => {
+			if (error) {
+				res.status(400).json({error: error});
+				return;
+			}
+			if(results.rows.length == 0){
+				res.status(404).json({message: "Student with name " + req.params.name + " and level " + req.params.level + " not found"});
+				return;
+			}
+			res.status(200).json(results.rows);
+		});
+	}
+}
+
+exports.deleteStudent = (req, res) => {
+	if(!req.body.name){
+		res.status(400).json({ message : "Please provide at least a name" });
+		return;
+	}
+
+	if(!req.body.level){
+		pool.query('DELETE FROM students WHERE name = $1', [req.body.name], (error, results) => {
+			if (error) {
+				res.status(400).json({error: error});
+				return;
+			}
+			if (results.rowCount == 0) {
+				res.status(404).json({ message : "Student not found with name: " + req.body.name });
+				return;
+			}
+			res.status(200).json( {message : `Student deleted with name: ${req.body.name}` })
+		});
+		return;
+	}
+
+	pool.query('DELETE FROM students WHERE name = $1 AND level = $2', [req.body.name, req.body.level], (error, results) => {
+		if (error) {
+			res.status(400).json({error: error});
+			return;
+		}
+		if (results.rowCount == 0) {
+			res.status(404).json({ message : "Student not found with name: " + req.body.name + " and level: " + req.body.level });
+			return;
+		}
+		res.status(200).json( {message : `Student deleted with name: ${req.body.name}, level: ${req.body.level}` })
+	}
+	);
+}
---
+++ routes/studentPostgres.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const studentCtrl = require("../controllers/studentPostgres");
+
+router
+    .post("/", studentCtrl.createStudent)
+    .get("/", studentCtrl.getStudents)
+    .get("/:name/:level", studentCtrl.getStudents)
+    .delete("/", studentCtrl.deleteStudent)
+;
+
+module.exports = router;
