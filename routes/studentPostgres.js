cefb136d7403daa4b6201f30a901d1ab9fb28ef7
SOLER Lilian <soler.lilian64@gmail.com>
2022-10-27 00:55:04 +0200

    Ajout student - only test get

--- routes/studentPostgres.js
+++ routes/studentPostgres.js
@@ -6,7 +6,8 @@ const studentCtrl = require("../controllers/studentPostgres");
 router
     .post("/", studentCtrl.createStudent)
     .get("/", studentCtrl.getStudents)
-    .get("/:name/:level", studentCtrl.getStudents)
+    .get("/:name", studentCtrl.getStudents)
+    .get("/name/all", studentCtrl.getAllStudentsName)
     .delete("/", studentCtrl.deleteStudent)
     .put("/", studentCtrl.updateStudent);
 ;
